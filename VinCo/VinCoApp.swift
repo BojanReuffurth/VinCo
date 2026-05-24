f6e24aa6768cd2725dccde643b78c5b4d882b25e
Bojan Reuffurth <bojan@reuffurth.ch>
2026-05-24 14:33:35 +0200

    Initial Commit

---
+++ VinCo/VinCoApp.swift
@@ -0,0 +1,32 @@
+//
+//  VinCoApp.swift
+//  VinCo
+//
+//  Created by Bojan Reuffurth on 24.05.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VinCoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
